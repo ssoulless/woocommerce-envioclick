26bd084ffb127221da686efa1e4ed29e835735c2
Sebastian Velandia <sebas.velandia27@gmail.com>
2020-04-24 15:54:40 -0500

    Added admin page and link

--- woocommerce-envioclick.php
+++ woocommerce-envioclick.php
@@ -19,16 +19,55 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( !class_exists('WC_Envioclick') ){
-	class WC_Envioclick{
+	class WC_Envioclick
+	{
+
+		public $plugin;
+
+		function __construct(){
+			$this->plugin = plugin_basename( __FILE__ );
+		}
     
 	    function register(){
 	    	add_action( 'admin_enqueue_scripts', array($this, 'enqueue') );
+	    	add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+	    	add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link') );
+	    }
+
+	    public function settings_link( $links ){
+	    	$settings_link = '<a href="admin.php?page=envioclick_plugin">Settings</a>';
+	    	array_push( $links, $settings_link);
+	    	return $links;
+	    }
+
+	    public function add_admin_pages(){
+	    	add_menu_page( 'EnvioClick', 'EnvioClick', 'manage_options', 'envioclick_plugin', array( $this, 'admin_index' ), 'dashicons-admin-generic', 9 );
+	    }
+
+	    public function admin_index(){
+	    	require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
 	    }
 
 	    function enqueue(){
 	    	// enqueue all the scripts
 	    	wp_enqueue_style( 'mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__) );
 	    }
+
+	 //    /*
+		// * Add action button in order list to change order status from completed to delivered
+		// */
+		// public function add_shipped_order_status_actions_button($actions, $order){
+			
+		// 	// wp_enqueue_style( 'shipment_tracking_styles',  wc_advanced_shipment_tracking()->plugin_dir_url() . 'assets/css/admin.css', array(), wc_advanced_shipment_tracking()->version );	
+		// 	// wp_enqueue_script( 'woocommerce-advanced-shipment-tracking-js', wc_advanced_shipment_tracking()->plugin_dir_url() . 'assets/js/admin.js', array( 'jquery' ), wc_advanced_shipment_tracking()->version);
+			
+		// 	$actions['add_tracking'] = array(
+		// 		'url'       => "#".$order->get_id(),
+		// 		'name'      => __( 'Add Tracking', 'woo-advanced-shipment-tracking' ),
+		// 		'action'    => "add_inline_tracking", // keep "view" class for a clean button CSS
+		// 	);		
+		// 	return $actions;
+		// }
 
 	     /**
 	     * Get MP alert frame for notfications 
