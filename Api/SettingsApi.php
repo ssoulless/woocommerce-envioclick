41e96ed8ca78c1e9555008695e10a4be08071138
Sebastian Velandia <sebas.velandia27@gmail.com>
2020-04-26 16:41:15 -0500

    added Settings API and adapted Admin controller

---
+++ Api/SettingsApi.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package EnvioclickPlugin
+ */
+namespace Inc\Api;
+
+class SettingsApi
+{
+	public $admin_pages = array();
+
+	public function register()
+	{
+		if ( ! empty( $this->admin_pages ) ) {
+			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+		}
+	}
+	
+	public function add_pages( array $pages )
+	{
+		$this->admin_pages = $pages;
+
+		return $this;
+	}
+
+	protected function add_admin_menu()
+	{
+		foreach ($this->admin_pages as $page) {
+			add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position'] );
+		}
+	}
+}
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -5,19 +5,38 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+	public $settings;
+
+	public $pages = [
+			[
+				'page_title' => 'EnvioClick', 
+				'menu_title' => 'EnvioClick', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'envioclick_plugin', 
+				'callback' => function() { echo '<h1>Envioclick Plugin</h1>'}, 
+				'icon_url' => 'dashicons-admin-generic', 
+				'position' => 9
+			]
+		];
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+	}
 
 	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+		$this->settings->add_pages( $this->pages )->register();
 	}
 
-	public function add_admin_pages(){
-		add_menu_page( 'EnvioClick', 'EnvioClick', 'manage_options', 'envioclick_plugin', array( $this, 'admin_index' ), 'dashicons-admin-generic', 9 );
-	}
+	// public function add_admin_pages(){
+	// 	add_menu_page( 'EnvioClick', 'EnvioClick', 'manage_options', 'envioclick_plugin', array( $this, 'admin_index' ), 'dashicons-admin-generic', 9 );
+	// }
 
-	public function admin_index(){
-		require_once $this->plugin_path . 'templates/admin.php';
-	}
+	// public function admin_index(){
+	// 	require_once $this->plugin_path . 'templates/admin.php';
+	// }
 }
