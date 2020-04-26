a1aa2f6060e479a0c8732f18a67da12388fea645
Sebastian Velandia <sebas.velandia27@gmail.com>
2020-04-26 18:29:17 -0500

    Fixed coding and syntax errors

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -32,16 +32,16 @@ class SettingsApi
 
 		$admin_page = $this->admin_pages[0];
 
-		$subpage = [
-			[
+		$subpage = array(
+			array(			
 				'parent_slug' => $admin_page['menu_slug'],
 				'page_title' => $admin_page['page_title'], 
 				'menu_title' => ($title) ? $title : $admin_page['menu_title'], 
 				'capability' => $admin_page['capability'], 
 				'menu_slug' => $admin_page['menu_slug'], 
 				'callback' => $admin_page['callback'],
-			]
-		];
+			)
+		);
 
 		$this->admin_subpages = $subpage;
 
@@ -56,14 +56,14 @@ class SettingsApi
 
 	}
 
-	protected function add_admin_menu()
+	public function add_admin_menu()
 	{
-		foreach ($this->admin_pages as $page) {
+		foreach ( $this->admin_pages as $page ) {
 			add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position'] );
 		}
 
-		foreach ($this->admin_subpages as $page) {
-			add_menu_page( $page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'] );
+		foreach ( $this->admin_subpages as $page ) {
+			add_submenu_page( $page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'] );
 		}
 	}
 }
